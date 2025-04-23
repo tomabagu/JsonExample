956060908014b92b515728707cd9bbb9660f44a0
Toma Sabulienė-Bagušė <toma.bagu@gmail.com>
2025-04-23 15:19:51 +0300

    fixed user.json file, deserialized all entries the json data into C# objects and output the data into the console

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace JsonExample
+﻿namespace JsonExample;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
 {
-    internal class Program
+    // Define a class to match the JSON structure
+    public class User
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        // Path to the local JSON file
+        string filePath = "user.json";
+
+        // Read the JSON data from the file
+        string jsonResponse = File.ReadAllText(filePath);
+
+        // Deserialize the JSON into a C# object
+        List<User> users = JsonConvert.DeserializeObject<List<User>>(jsonResponse);
+
+        // Display the user's information
+        foreach (User user in users)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"Age: {user.Age}");
+            Console.WriteLine($"City: {user.City}");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1.0v added user.json
 1.1v added new entries to the user.json file
+1.2v fixed user.json file, deserialized all entries the json data into C# objects and output the data into the console
